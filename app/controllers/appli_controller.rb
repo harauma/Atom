68e6a7057e5002f3aa3f4541c227ec309b2f8e18
kai <k.shirookka@gmail.com>
2019-11-20 02:51:03 +0000

    申請書登録処理途中

--- app/controllers/appli_controller.rb
+++ app/controllers/appli_controller.rb
@@ -6,9 +6,26 @@ class AppliController < ApplicationController
     @name = params[:name]
     @place = params[:place]
     @prefecture = params[:prefecture]
+    @s_date = params[:s_date]
+    @e_date = params[:e_date]
+    @d_date = params[:d_date]
     @s_time = params[:s_time]
     @e_time = params[:e_time]
     @d_time = params[:d_time]
+    @appli = Appli.new(appli_params)
+
+  end
+
+  def submit
+    if @appli.save
+      redirect_to('/')
+    else
+      render('new')
+    end
+  end
+
+  def appli_params
+    params.require(:appli).permit(:company_name,:place,:cantent,:start,:end,:withdrawal_day,:withdrawal,:admit_flg,:watch_flg)
   end
 
 end
